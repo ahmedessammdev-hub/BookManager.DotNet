43c091cf1ce4b927820e832d5d848b785436ccf2
Ahmed Essam <ahmed.essam.m.dev@gmail.com>
2026-06-23 19:20:00 +0300

    feat(session-4): add Isbn property to Book entity with database migration

---
+++ session-4/Entities/Book.cs
@@ -0,0 +1,12 @@
+namespace tasks.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public int PageCount { get; set; }
+    public string Isbn { get; set; } = string.Empty;
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+}
